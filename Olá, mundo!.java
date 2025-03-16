764a8c041c9b3e4e8a83d6b7607c3664fd3060e2
debzita ୨ৎ <debzitaaaa@gmail.com>
2025-03-16 17:26:46 -0300

    Olá, mundo!.java
    Meu primeiro programa no java <3

---
+++ Olá, mundo!.java
@@ -0,0 +1,6 @@
+package primeiroprograma; //pacote
+public class PrimeiroPrograma { //classe
+  public static void main(String[] args){ //classe principal 
+    System.out.println("Olá, Mundo!"); //print no java
+  }
+}
