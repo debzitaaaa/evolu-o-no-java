c3d6e6c22ff762759f39d3b10cedf10271236ab2
debzita ୨ৎ <debzitaaaa@gmail.com>
2025-03-16 17:19:37 -0300

    conceitos iniciais.java

---
+++ conceitos iniciais.java
@@ -0,0 +1,5 @@
+// Aprenddi alguns conceitos iniciais no java, como a sua história, muito interessante por sinal (Eu não fazia a mínima ideia que java significava café forte, POR MAIS QUE A LOGO DO JAVA SEJA UM CAFÉ(???))
+//Aprendi também sobre o Javac - compilador do Java, responsável por transformar o código fonte em um bytecode- mas esse código gerado pelo compilador só vai ser executado quando passar pela JVM(uma máquina virtual do java). 
+//E basicamente existem dois tipos de usuários do Java, os JRE, que são aqueles que apenas consomem, e esses tem a JVM e Bibliotecas. Também existem os JDK, que são aqueles que desenvovlem através do Java, eles possuem o JRE, JavaLang, Javatools, Javac e o Debugger. 
+//Eu penso na JVM como se fosse tipo um processador do Java sabe? Nele existem várias partezinhas, como o interpretador, loader, verificador, gerenciador e etc...
+//A versão do Java que atualmente uso é o Java JDK SE  23.0.2 :) 
